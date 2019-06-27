f417726bc3e196132f83ff830ffcf69dbb57f04b
Rodrigo Andrade <rodrigoandrade@fb.com>
2019-06-27 16:39:36 -0700

    All required + Movie Poster Grid Layout Collection View

---
+++ Flix Application/View Controllers/DetailsViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailsViewController.h
+//  Flix Application
+//
+//  Created by rodrigoandrade on 6/26/19.
+//  Copyright © 2019 rodrigoandrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flix Application/Views/MovieCollectionCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionCell.m
+//  Flix Application
+//
+//  Created by rodrigoandrade on 6/27/19.
+//  Copyright © 2019 rodrigoandrade. All rights reserved.
+//
+
+#import "MovieCollectionCell.h"
+
+@implementation MovieCollectionCell
+
+@end
